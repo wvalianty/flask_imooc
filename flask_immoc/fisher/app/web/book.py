6beaad09359e9095cf25c112f8859daf6b5ea735
root <root@DESKTOP-ETNKUVH.localdomain>
2018-06-30 12:39:42 +0800

    注册蓝图

---
+++ flask_immoc/fisher/app/web/book.py
@@ -0,0 +1,15 @@
+from flask import jsonify,Blueprint
+from helper import is_isbn_or_key
+from httper import HTTP
+from yushu_book import YuShuBook
+
+web = Blueprint('web','__name__')
+
+@web.route("/book/search/<q>/<page>")
+def search(q,page):
+    isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    if isbn_or_key == 'key':
+        result = YuShuBook.search_by_keyword(q)
+    return jsonify(result)
